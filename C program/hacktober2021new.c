82a7eda2231505db48f56aafd2115b66d0dab30d
warunasrinath <mvwsrinath@students.nsbm.ac.lk>
2021-10-22 15:05:29 +0530

    Added new file to this repository again

--- C program/hacktober2021new.c
+++ C program/hacktober2021new.c
@@ -1,6 +1,7 @@
 int main()
 {
-    printf("Welcome to Hacktober 2021!\n");
+    printf("Welcome to Hacktober 2021 !\n");
+    printf("Created new_branch in Learn-Share-Hacktoberfest2021\n");
     getch(); 
     return 0;
 }
